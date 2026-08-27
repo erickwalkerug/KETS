b3544599fe8bd5da1010254a7b1dd9761d3480c9
erickwalkerug <erickwalkerug@gmail.com>
2026-08-27 20:49:06 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="kets-v2-20260827";
-const ASSETS=["/","/index.html","/styles.css","/app.js","/manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin!==self.location.origin||u.pathname.startsWith("/api/"))return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match(e.request).then(x=>x||caches.match("/index.html"))));});
